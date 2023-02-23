f32db8240f608eaf0145fa11b9f74203d554bf0d
Dromero83 <89482575+Dromero83@users.noreply.github.com>
2023-02-22 21:53:38 -0600

    update

--- app.js
+++ app.js
@@ -5,9 +5,30 @@ const day = days[d.getDay()];
 document.getElementById("itsYear").innerHTML = "It is " + day + " my dudes";
 
 const gems = ["Ruby", "Sapphire", "Diamond", "Emerald", "Opal", "Amethyst", "Garnet",];
-gems.sort();
-const gemJoin = gems.join(", ")
+gemJoin = gems.join(", ")
 
 document.getElementById("gemSort").innerHTML = gemJoin;
 
-//remember to add method buttons later!
+function sorting() {
+    gems.sort();
+    gemNew = gems.join(", ")
+    document.getElementById("gemSort").innerHTML = gemNew;
+  }
+
+function Laptop(type, year, color){
+    this.type = type;
+    this.year = year;
+    this.color = color;
+    this.changeLap = function (type, color) {
+        this.type = type;
+        this.color = color;
+      }
+}
+const myLaptop = new Laptop("Windows XP", "2001", "black")
+
+document.getElementById("myLaptop").innerHTML = `I own a ${myLaptop.color} ${myLaptop.type} laptop. It's from ${myLaptop.year}.`;
+
+function upLap(){
+    myLaptop.changeLap("Windows 10", "red");
+    document.getElementById("myLaptop").innerHTML = `I now own a ${myLaptop.color} ${myLaptop.type} laptop.`;
+}
